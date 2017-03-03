d3e3c5c658356bc8dd3d1602d95837ecc2e93076
Zac Mitton <zacmitton22@gmail.com>
2017-03-02 21:19:18 -0500

    bare minimum contracts

--- lib.js
+++ lib.js
@@ -1,18 +1,3 @@
-// const Contract = require('truffle-contract')
-// const Web3 = require('web3');
-// const Uport = {
-//   ArrayLib:                       Contract(require('./build/contracts/ArrayLib.json')),
-//   Migrations:                     Contract(require('./build/contracts/Migrations.json')),
-//   Owned:                          Contract(require('./build/contracts/Owned.json')),
-//   Proxy:                          Contract(require('./build/contracts/Proxy.json')),
-//   RecoveryQuorum:                 Contract(require('./build/contracts/RecoveryQuorum.json')),
-//   StandardController:             Contract(require('./build/contracts/StandardController.json')),
-//   Registry:                       Contract(require('./build/contracts/UportRegistry.json'))
-// }
-
-// module.exports = { Uport , Web3 }
-
-
 const Contract = require('truffle-contract')
 Web3 = require('web3');
 Uport = {
@@ -24,4 +9,3 @@ Uport = {
   StandardController:             Contract(require('./build/contracts/StandardController.json')),
   Registry:                       Contract(require('./build/contracts/UportRegistry.json'))
 }
-
