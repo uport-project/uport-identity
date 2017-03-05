612554eb1eaf91bbe2ebcebfaee286f47c6adc26
Joel Torstensson <oed3933@gmail.com>
2017-03-05 15:27:11 +0100

    Added identity factories

---
+++ contracts/factories/IdentityFactory.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.4;
+import "./RecoveryQuorum.sol";
+
+contract IdentityFactory {
+    event IdentityCreated(
+        address indexed userKey,
+        address proxy,
+        address controller,
+        address recoveryQuorum);
+
+    mapping(address => address) public senderToProxy;
+
+    //cost ~2.4M gas
+    function CreateProxyWithControllerAndRecovery(address userKey, address[] delegates, uint longTimeLock, uint shortTimeLock) {
+        Proxy proxy = new Proxy();
+        RecoverableController controller = new RecoverableController(proxy, userKey, longTimeLock, shortTimeLock);
+        proxy.transfer(controller);
+        RecoveryQuorum recoveryQuorum = new RecoveryQuorum(controller, delegates);
+        controller.changeRecoveryFromRecovery(recoveryQuorum);
+
+        IdentityCreated(userKey, proxy, controller, recoveryQuorum);
+        senderToProxy[msg.sender] = proxy;
+    }
+}
---
+++ contracts/factories/IdentityFactoryWithRecoveryKey.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.4;
+import "./RecoverableController.sol";
+
+contract IdentityFactoryWithRecoveryKey {
+    event IdentityCreated(
+        address indexed userKey,
+        address proxy,
+        address controller,
+        address recoveryKey);
+
+    mapping(address => address) public senderToProxy;
+
+    //cost ~2.4M gas
+    function CreateProxyWithControllerAndRecoveryKey(address userKey, address _recoveryKey, uint longTimeLock, uint shortTimeLock) {
+        Proxy proxy = new Proxy();
+        RecoverableController controller = new RecoverableController(proxy, userKey, longTimeLock, shortTimeLock);
+        proxy.transfer(controller);
+        controller.changeRecoveryFromRecovery(_recoveryKey);
+
+        IdentityCreated(userKey, proxy, controller, _recoveryKey);
+        senderToProxy[msg.sender] = proxy;
+    }
+}
