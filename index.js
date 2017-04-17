5dfc65332d0fd617aec4f32db55e16d677daa641
Pelle Braendgaard <pelle@stakeventures.com>
2017-04-16 21:46:40 -0600

    IdentitManager which creates identities and acts as a controller

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   ArrayLib:                       require('./build/contracts/ArrayLib.json'),
   IdentityFactory:                require('./build/contracts/IdentityFactory.json'),
   IdentityFactoryWithRecoveryKey: require('./build/contracts/IdentityFactoryWithRecoveryKey.json'),
+  IdentityManager:                require('./build/contracts/IdentityManager.json'),
   RecoverableController:          require('./build/contracts/RecoverableController.json'),
   Proxy:                          require('./build/contracts/Proxy.json'),
   RecoveryQuorum:                 require('./build/contracts/Proxy.json'),
