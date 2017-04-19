3cbbb677d1effc3ae35241e81fa7b0b099f8a634
Joel Torstensson <oed3933@gmail.com>
2017-04-19 17:36:22 +0200

    Added essential contracts to index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = {
   IdentityFactory:                require('./build/contracts/IdentityFactory.json'),
   IdentityFactoryWithRecoveryKey: require('./build/contracts/IdentityFactoryWithRecoveryKey.json'),
+  RecoverableController:          require('./build/contracts/RecoverableController.json'),
+  Proxy:                          require('./build/contracts/Proxy.json'),
+  RecoveryQuorum:                 require('./build/contracts/Proxy.json'),
 }
