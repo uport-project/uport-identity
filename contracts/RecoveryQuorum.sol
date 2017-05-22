0165ede539a2f54c547ddad753f5257dd8cc87f3
Joel Torstensson <oed3933@gmail.com>
2017-05-22 20:40:43 +0200

    Code cleanup of RecoveryQuorum

--- contracts/RecoveryQuorum.sol
+++ contracts/RecoveryQuorum.sol
@@ -3,120 +3,134 @@ import "./RecoverableController.sol";
 import "./libs/ArrayLib.sol";
 
 contract RecoveryQuorum {
-  RecoverableController public controller;
-  uint constant MAX_DELEGATES = 15;
+    RecoverableController public controller;
+    uint constant MAX_DELEGATES = 15;
 
-  address[] public delegateAddresses; // needed for iteration of mapping
-  mapping (address => Delegate) public delegates;
-  struct Delegate{
-    uint deletedAfter; // delegate exists if not 0
-    uint pendingUntil;
-    address proposedUserKey;
-  }
-
-  event RecoveryEvent(string action, address initiatedBy);
-
-  modifier onlyUserKey(){ if (msg.sender == controller.userKey()) _; }
-
-  function RecoveryQuorum(address _controller, address[] _delegates){
-    controller = RecoverableController(_controller);
-    for(uint i = 0; i < MAX_DELEGATES; i++){
-      delegateAddresses.push(_delegates[i]);
-      delegates[_delegates[i]] = Delegate({proposedUserKey: 0x0, pendingUntil: 0, deletedAfter: 31536000000000});
+    address[] public delegateAddresses; // needed for iteration of mapping
+    mapping (address => Delegate) public delegates;
+    struct Delegate {
+        uint deletedAfter; // delegate exists if not 0
+        uint pendingUntil;
+        address proposedUserKey;
     }
-  }
 
-  function signUserChange(address proposedUserKey) {
-    if(delegateRecordExists(delegates[msg.sender])) {
-      delegates[msg.sender].proposedUserKey = proposedUserKey;
-      changeUserKey(proposedUserKey);
-      RecoveryEvent("signUserChange", msg.sender);
-    }
-  }
+    event RecoveryEvent(string action, address initiatedBy);
 
-  function changeUserKey(address newUserKey) {
-    if(collectedSignatures(newUserKey) >= neededSignatures()){
-      controller.changeUserKeyFromRecovery(newUserKey);
-      for(uint i = 0 ; i < delegateAddresses.length ; i++){
-        //remove any pending delegates after a recovery
-        if(delegates[delegateAddresses[i]].pendingUntil > now){ 
-            delegates[delegateAddresses[i]].deletedAfter = now;
+    modifier onlyUserKey(){ if (msg.sender == controller.userKey()) _; }
+
+    function RecoveryQuorum(address _controller, address[] _delegates) {
+        controller = RecoverableController(_controller);
+        for(uint i = 0; i < MAX_DELEGATES; i++) {
+            delegateAddresses.push(_delegates[i]);
+            delegates[_delegates[i]] = Delegate({
+                proposedUserKey: 0x0,
+                pendingUntil: 0,
+                deletedAfter: 31536000000000
+            });
         }
-        delete delegates[delegateAddresses[i]].proposedUserKey;
-      }
     }
-  }
 
-  function replaceDelegates(address[] delegatesToRemove, address[] delegatesToAdd) onlyUserKey{
-    for(uint i = 0 ; i < delegatesToRemove.length ; i++){
-      removeDelegate(delegatesToRemove[i]);
+    function signUserChange(address proposedUserKey) {
+        if(delegateRecordExists(delegates[msg.sender])) {
+            delegates[msg.sender].proposedUserKey = proposedUserKey;
+            changeUserKey(proposedUserKey);
+            RecoveryEvent("signUserChange", msg.sender);
+        }
     }
-    garbageCollect();
-    for(uint j = 0 ; j < delegatesToAdd.length ; j++){
-      addDelegate(delegatesToAdd[j]);
+
+    function changeUserKey(address newUserKey) {
+        if(collectedSignatures(newUserKey) >= neededSignatures()) {
+            controller.changeUserKeyFromRecovery(newUserKey);
+            for(uint i = 0 ; i < delegateAddresses.length ; i++) {
+                //remove any pending delegates after a recovery
+                if(delegates[delegateAddresses[i]].pendingUntil > now) {
+                    delegates[delegateAddresses[i]].deletedAfter = now;
+                }
+                delete delegates[delegateAddresses[i]].proposedUserKey;
+            }
+        }
     }
-    RecoveryEvent("replaceDelegates", msg.sender);
-  }
 
-  function collectedSignatures(address _proposedUserKey) returns (uint signatures){
-    for(uint i = 0 ; i < delegateAddresses.length ; i++){
-      if (delegateHasValidSignature(delegates[delegateAddresses[i]]) && delegates[delegateAddresses[i]].proposedUserKey == _proposedUserKey){
-        signatures++;
-      }
+    function replaceDelegates(address[] delegatesToRemove, address[] delegatesToAdd) onlyUserKey {
+        for(uint i = 0 ; i < delegatesToRemove.length ; i++) {
+            removeDelegate(delegatesToRemove[i]);
+        }
+        garbageCollect();
+        for(uint j = 0 ; j < delegatesToAdd.length ; j++) {
+            addDelegate(delegatesToAdd[j]);
+        }
+        RecoveryEvent("replaceDelegates", msg.sender);
     }
-  }
 
-  function getAddresses() constant returns (address[]){ return delegateAddresses; }
-
-  function neededSignatures() returns (uint){
-    uint currentDelegateCount; //always 0 at this point
-    for(uint i = 0 ; i < delegateAddresses.length ; i++){
-      if(delegateIsCurrent(delegates[delegateAddresses[i]])){ currentDelegateCount++; }
+    function collectedSignatures(address _proposedUserKey) returns (uint signatures) {
+        for(uint i = 0 ; i < delegateAddresses.length ; i++) {
+            if (delegateHasValidSignature(delegates[delegateAddresses[i]])
+                    && delegates[delegateAddresses[i]].proposedUserKey == _proposedUserKey) {
+                signatures++;
+            }
+        }
     }
-    return currentDelegateCount/2 + 1;
-  }
 
-  function addDelegate(address delegate) private {
-    if(!delegateRecordExists(delegates[delegate]) && delegateAddresses.length < MAX_DELEGATES) {
-      delegates[delegate] = Delegate({proposedUserKey: 0x0, pendingUntil: now + controller.longTimeLock(), deletedAfter: 31536000000000});
-      delegateAddresses.push(delegate);
+    function getAddresses() constant returns (address[]) {
+        return delegateAddresses;
     }
-  }
 
-  function removeDelegate(address delegate) private {
-    if(delegates[delegate].deletedAfter > controller.longTimeLock() + now) {
-      //remove right away if they are still pending
-      if(delegates[delegate].pendingUntil > now) {
-        delegates[delegate].deletedAfter = now;
-      } else {
-        delegates[delegate].deletedAfter = controller.longTimeLock() + now;
-      }
+    function neededSignatures() returns (uint) {
+        uint currentDelegateCount; //always 0 at this point
+        for(uint i = 0 ; i < delegateAddresses.length ; i++) {
+            if(delegateIsCurrent(delegates[delegateAddresses[i]])) {
+                currentDelegateCount++;
+            }
+        }
+        return currentDelegateCount/2 + 1;
     }
-  }
 
-  function garbageCollect() private {
-    uint i = 0;
-    while(i < delegateAddresses.length) {
-      if(delegateIsDeleted(delegates[delegateAddresses[i]])) {
-        delete delegates[delegateAddresses[i]];
-        ArrayLib.removeAddress(i, delegateAddresses);
-      } else { i++; }
+    function addDelegate(address delegate) private {
+        if(!delegateRecordExists(delegates[delegate])
+                && delegateAddresses.length < MAX_DELEGATES) {
+            delegates[delegate] = Delegate({
+                proposedUserKey: 0x0,
+                pendingUntil: now + controller.longTimeLock(),
+                deletedAfter: 31536000000000
+            });
+            delegateAddresses.push(delegate);
+        }
     }
-  }
 
-  function delegateRecordExists(Delegate d) private returns (bool){
-      return d.deletedAfter != 0;
-  }
+    function removeDelegate(address delegate) private {
+        if(delegates[delegate].deletedAfter > controller.longTimeLock() + now) {
+            //remove right away if they are still pending
+            if(delegates[delegate].pendingUntil > now) {
+                delegates[delegate].deletedAfter = now;
+            } else {
+                delegates[delegate].deletedAfter = controller.longTimeLock() + now;
+            }
+        }
+    }
 
-  function delegateIsDeleted(Delegate d) private returns (bool){
-      return d.deletedAfter <= now; //doesnt check record existence
-  }
+    function garbageCollect() private {
+        uint i = 0;
+        while(i < delegateAddresses.length) {
+            if(delegateIsDeleted(delegates[delegateAddresses[i]])) {
+                delete delegates[delegateAddresses[i]];
+                ArrayLib.removeAddress(i, delegateAddresses);
+            } else { i++; }
+        }
+    }
 
-  function delegateIsCurrent(Delegate d) private returns (bool){
-      return delegateRecordExists(d) && !delegateIsDeleted(d) && now > d.pendingUntil;
-  }
+    function delegateRecordExists(Delegate d) private returns (bool) {
+        return d.deletedAfter != 0;
+    }
 
-  function delegateHasValidSignature(Delegate d) private returns (bool){
-      return delegateIsCurrent(d) && d.proposedUserKey != 0x0;
-  }
+    function delegateIsDeleted(Delegate d) private returns (bool) {
+        return d.deletedAfter <= now; //doesnt check record existence
+    }
+
+    function delegateIsCurrent(Delegate d) private returns (bool) {
+        return delegateRecordExists(d) && !delegateIsDeleted(d) && now > d.pendingUntil;
+    }
+
+    function delegateHasValidSignature(Delegate d) private returns (bool) {
+        return delegateIsCurrent(d) && d.proposedUserKey != 0x0;
+    }
 }
