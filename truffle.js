a3a551e9888e5f1e7bc5fde7c85fe1a3e7a664f8
Joel Torstensson <oed3933@gmail.com>
2017-05-22 20:06:54 +0200

    Simplified code

--- truffle.js
+++ truffle.js
@@ -2,7 +2,9 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const TestRPC = require("ethereumjs-testrpc");
 
-function getNmemonic(){
+let provider
+
+function getNmemonic() {
   try{
     return require('fs').readFileSync("./seed", "utf8").trim();
   } catch(err){
@@ -10,7 +12,13 @@ function getNmemonic(){
   }
 }
 
-let provider
+function getProvider(rpcUrl) {
+  if (!provider) {
+    provider = new HDWalletProvider(getNmemonic(), rpcUrl)
+  }
+  return provider
+}
+
 
 module.exports = {
   networks: {
@@ -30,46 +38,31 @@ module.exports = {
     },
     ropsten: {
       get provider() {
-        if (!provider) {
-          provider = new HDWalletProvider(getNmemonic(), "https://ropsten.infura.io/")
-        }
-        return provider
+        return getProvider("https://ropsten.infura.io/")
       },
       network_id: 3
     },
     rinkeby: {
       get provider() {
-        if (!provider) {
-          provider = new HDWalletProvider(getNmemonic(), "https://rinkeby.infura.io/")
-        }
-        return provider
+        return getProvider("https://rinkeby.infura.io/")
       },
       network_id: 4
     },
     infuranet: {
       get provider() {
-        if (!provider) {
-          provider = new HDWalletProvider(getNmemonic(), "https://infuranet.infura.io/")
-        }
-        return provider
+        return getProvider("https://infuranet.infura.io/")
       },
       network_id: "*"
     },
     kovan: {
       get provider() {
-        if (!provider) {
-          provider = new HDWalletProvider(getNmemonic(), "https://kovan.infura.io/")
-        }
-        return provider
+        return getProvider("https://kovan.infura.io/")
       },
       network_id: 42
     },
     mainnet: {
       get provider() {
-        if (!provider) {
-          provider = new HDWalletProvider(getNmemonic(), "https://mainnet.infura.io/")
-        }
-        return provider
+        return getProvider("https://mainnet.infura.io/")
       },
       network_id: 1
     }
