03a546d137e8d518fa4538b05d7e3951949725b5
Pelle Braendgaard <pelle@stakeventures.com>
2017-05-23 21:15:14 -0600

    Configurable time locks

--- contracts/IdentityManager.sol
+++ contracts/IdentityManager.sol
@@ -2,6 +2,10 @@ pragma solidity 0.4.8;
 import "./Proxy.sol";
 
 contract IdentityManager {
+  uint adminTimeLock;
+  uint userTimeLock;
+  uint adminRate;
+
   event IdentityCreated(
     address indexed identity,
     address indexed creator,
@@ -28,12 +32,12 @@ contract IdentityManager {
   mapping(address => uint) limiter;
 
   modifier onlyOwner(address identity) { 
-    if (owners[identity][msg.sender] > 0 && (owners[identity][msg.sender] + 1 hours) <= now ) _ ;
+    if (owners[identity][msg.sender] > 0 && (owners[identity][msg.sender] + userTimeLock) <= now ) _ ;
     else throw; 
   }
 
   modifier onlyOlderOwner(address identity) { 
-    if (owners[identity][msg.sender] > 0 && (owners[identity][msg.sender] + 1 days) <= now) _ ;
+    if (owners[identity][msg.sender] > 0 && (owners[identity][msg.sender] + adminTimeLock) <= now) _ ;
     else throw;
   }
 
@@ -43,17 +47,27 @@ contract IdentityManager {
   }
 
   modifier rateLimited() {
-    if (limiter[msg.sender] < (now - 1 hours)) {
+    if (limiter[msg.sender] < (now - adminRate)) {
       limiter[msg.sender] = now;
       _ ;
     } else throw;
+  }
+
+  // Instantiate IdentityManager with the following limits:
+  // - userTimeLock - Time before new owner can control proxy
+  // - adminTimeLock - Time before new owner can add/remove owners
+  // - adminRate - Time period used for rate limiting a given key for admin functionality
+  function IdentityManager(uint _userTimeLock, uint _adminTimeLock, uint _adminRate) {
+    adminTimeLock = _adminTimeLock;
+    userTimeLock = _userTimeLock;
+    adminRate = _adminRate;
   }
 
   // Factory function
   // gas 289,311
   function CreateIdentity(address owner, address recoveryKey) {
     Proxy identity = new Proxy();
-    owners[identity][owner] = now - 1 days; // This is to ensure original owner has full power from day one
+    owners[identity][owner] = now - adminTimeLock; // This is to ensure original owner has full power from day one
     recoveryKeys[identity] = recoveryKey;
     IdentityCreated(identity, msg.sender, owner,  recoveryKey);
   }
@@ -62,26 +76,26 @@ contract IdentityManager {
   // Note they also have to change the owner of the Proxy over to this, but after calling this
   function registerIdentity(address owner, address recoveryKey) {
     if (owners[msg.sender][owner] > 0 || recoveryKeys[msg.sender] > 0 ) throw; // Deny any funny business
-    owners[msg.sender][owner] = now - 1 days; // This is to ensure original owner has full power from day one
+    owners[msg.sender][owner] = now - adminTimeLock; // This is to ensure original owner has full power from day one
     recoveryKeys[msg.sender] = recoveryKey;
     IdentityCreated(msg.sender, msg.sender, owner, recoveryKey);
   }
 
-
+  // Primary forward function
   function forwardTo(Proxy identity, address destination, uint value, bytes data) onlyOwner(identity) {
     identity.forward(destination, value, data);
   }
 
   // an owner can add a new device instantly
   function addOwner(Proxy identity, address newOwner) onlyOlderOwner(identity) rateLimited {
-    owners[identity][newOwner] = now + 1 days;
+    owners[identity][newOwner] = now + adminTimeLock;
     OwnerAdded(identity, newOwner, msg.sender);
   }
 
   // a recovery key owner can add a new device with 1 days wait time
   function addOwnerForRecovery(Proxy identity, address newOwner) onlyRecovery(identity) rateLimited {
     if (owners[identity][newOwner] > 0) throw;
-    owners[identity][newOwner] = now + 1 days;
+    owners[identity][newOwner] = now + adminTimeLock;
     OwnerAdded(identity, newOwner, msg.sender);
   }
 
