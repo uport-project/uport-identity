7f84d743be0789f03aed5fc9b9891bf497f5f0cb
Joel Torstensson <oed3933@gmail.com>
2017-05-31 08:20:43 +0200

    Fixed ref to recovery quorum in indes.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ module.exports = {
   IdentityFactoryWithRecoveryKey: require('./build/contracts/IdentityFactoryWithRecoveryKey.json'),
   RecoverableController:          require('./build/contracts/RecoverableController.json'),
   Proxy:                          require('./build/contracts/Proxy.json'),
-  RecoveryQuorum:                 require('./build/contracts/Proxy.json'),
+  RecoveryQuorum:                 require('./build/contracts/RecoveryQuorum.json'),
 }
