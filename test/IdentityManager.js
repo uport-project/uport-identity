fd749c0412dcf7b0e46e666a0d6f677ce4e24fb7
Nate Rush <narush@wharton.upenn.edu>
2017-06-08 12:17:23 -0400

    Migration Tests
    Added tests for migration. Would love some feedback on them :~)

--- test/IdentityManager.js
+++ test/IdentityManager.js
@@ -392,4 +392,153 @@ contract('IdentityManager', (accounts) => {
       })
     })
   })
+  describe('migration', () => {
+    let newIdenManager
+    beforeEach(done => {
+      identityManager.CreateIdentity(user1, recoveryKey, {from: nobody}).then(tx => {
+        const log = tx.logs[0]
+        assert.equal(log.event, 'IdentityCreated', 'wrong event')
+        proxy = Proxy.at(log.args.identity)
+        return IdentityManager.new(userTimeLock, adminTimeLock, adminRate)
+      }).then((instance) => {
+        newIdenManager = instance
+        //user2 is now a younger owner, while user1 is an olderowner
+        return identityManager.addOwner(proxy.address, user2)
+      }).then(tx => {
+        let log = tx.logs[0]
+        assert.equal(log.event, 'OwnerAdded', 'wrong event')
+        assert.equal(log.args.identity, proxy.address, 'wrong proxy')
+        assert.equal(log.args.owner, user2, 'wrong owner added')
+        assert.equal(log.args.instigator, user1, 'wrong initiator')
+        done()
+      }).catch(e => {
+        console.log(e)
+        done()
+      })
+    })
+
+    it('correct keys can start transfer', (done) => {
+      //olderOwner should be able to start transfer
+      identityManager.initiateMigration(proxy.address, newIdenManager.address, {from: user1}).then(tx => {
+        let log = tx.logs[0]
+        assert.equal(log.event, 'MigrationInitiated', 'wrong event initiated')
+        assert.equal(log.args.identity, proxy.address, 'migrating wrong proxy')
+        assert.equal(log.args.newIdManager, newIdenManager.address, 'migrating to wrong location')
+        assert.equal(log.args.instigator, user1, 'migrating to wrong location')
+        //young owner should not be able to start transfer
+        return identityManager.initiateMigration(proxy.address, newIdenManager.address, {from: user2})
+      }).then(tx => {
+        assert.isFalse(true, 'Should have thrown an error here')
+      }).catch((error) => {
+        assert.isTrue(error.toString().indexOf("invalid JUMP") != -1, 'Should have thrown, did not')
+      }).then(() => {
+        //non-owner should not be able to start transfer
+        return identityManager.initiateMigration(proxy.address, newIdenManager.address, {from: nobody})
+      }).then(() => {
+        assert.isFalse(true, 'Should have thrown an error here')
+      }).catch((error) => {
+        assert.isTrue(error.toString().indexOf("invalid JUMP") != -1, 'Should have thrown, did not')
+        done()
+      })
+    })
+
+    it('correct keys can cancel migration', (done) => {
+      identityManager.initiateMigration(proxy.address, newIdenManager.address, {from: user1}).then(tx => {
+        let log = tx.logs[0]
+        assert.equal(log.event, 'MigrationInitiated', 'wrong event initiated')
+        assert.equal(log.args.identity, proxy.address, 'migrating wrong proxy')
+        assert.equal(log.args.newIdManager, newIdenManager.address, 'migrating to wrong location')
+        assert.equal(log.args.instigator, user1, 'started migrating from wrong user')
+        return identityManager.cancelMigration(proxy.address, {from: user1})
+      }).then(tx => {
+        let log = tx.logs[0]
+        assert.equal(log.event, 'MigrationCanceled', 'wrong event initiated')
+        assert.equal(log.args.identity, proxy.address, 'canceled migrating wrong proxy')
+        assert.equal(log.args.newIdManager, newIdenManager.address, 'canceled migration to wrong location')
+        assert.equal(log.args.instigator, user1, 'canceled migrating from wrong user')
+        //set up migration again
+        return identityManager.initiateMigration(proxy.address, newIdenManager.address, {from: user1})
+      }).then(tx => {
+        //Second migration attempt, should allow
+        let log = tx.logs[0]
+        assert.equal(log.event, 'MigrationInitiated', 'wrong event initiated')
+        assert.equal(log.args.identity, proxy.address, 'migrating wrong proxy')
+        assert.equal(log.args.newIdManager, newIdenManager.address, 'migrating to wrong location')
+        assert.equal(log.args.instigator, user1, 'started migrating from wrong person')
+        return identityManager.cancelMigration(proxy.address, {from: user2})
+      }).then(tx => {
+        //young owner should also be able to cancel migration
+        let log = tx.logs[0]
+        assert.equal(log.event, 'MigrationCanceled', 'wrong event initiated')
+        assert.equal(log.args.identity, proxy.address, 'canceled migrating wrong proxy')
+        assert.equal(log.args.newIdManager, newIdenManager.address, 'canceled migration to wrong location')
+        assert.equal(log.args.instigator, user2, 'canceled migrating from wrong person')
+        return identityManager.initiateMigration(proxy.address, newIdenManager.address, {from: user1})
+      }).then(() => {
+        //Don't need to check setup again
+        return identityManager.cancelMigration(proxy.address, {from: nobody})
+      }).then(tx => {
+        assert.isFalse(true, 'Should have thrown error')
+      }).catch(error => {
+        assert.isTrue(error.toString().indexOf("invalid JUMP") != -1, 'Should have thrown, did not')
+      }).then(() => {
+        done()
+      })
+    })
+
+    it('correct keys should finilize transfer', (done) => {
+      identityManager.initiateMigration(proxy.address, newIdenManager.address, {from: user1}).then(tx => {
+        return identityManager.finalizeMigration(proxy.address, {from: nobody})
+      }).then(() => {
+        assert.isFalse(true, 'non-owner should not be able to finalize')
+      }).catch(error => {
+        assert.isTrue(error.toString().indexOf("invalid JUMP") != -1, 'should have thrown, did not')
+        return identityManager.finalizeMigration(proxy.address, {from: user2})
+      }).then(() => {
+        assert.isFalse(true, 'young owner should not be able to finalize')
+        //has to be greater than the adminTimeLock past to be finalized
+      }).catch(error => {
+        assert.isTrue(error.toString().indexOf("invalid JUMP") != -1, 'should have thrown, did not')
+      }).then(() => {
+        evm_increaseTime(2 * adminTimeLock)
+      }).then(() => {
+        return identityManager.finalizeMigration(proxy.address, {from: user1})
+      }).then(tx => {
+        //console.log(tx.recipt)
+        let log = tx.logs[0]
+        assert.equal(log.event, 'MigrationFinalized', 'wrong event initiated')
+        assert.equal(log.args.identity, proxy.address, 'finalized migrating wrong proxy')
+        assert.equal(log.args.newIdManager, newIdenManager.address, 'finalized migration to wrong location')
+        assert.equal(log.args.instigator, user1, 'finalized migrating from wrong person')
+      }).then(() => {
+        done()
+      })
+    })
+
+    it('should be owner of new identityManager after successful transfer', (done) => {
+      identityManager.initiateMigration(proxy.address, newIdenManager.address, {from: user1}).then(tx => {
+        let data = '0x' + lightwallet.txutils._encodeFunctionTxData('registerIdentity', ['address', 'address'], [user1, recoveryKey])
+        return identityManager.forwardTo(proxy.address, newIdenManager.address, 0, data, {from: user1})
+      }).then(() => {
+        //increase time until migration can be finialized
+        evm_increaseTime(2 * adminTimeLock)
+      }).then(() => {
+        return identityManager.finalizeMigration(proxy.address, newIdenManager.address, {from: user1})
+      }).then(tx => {
+        let log = tx.logs[0]
+        assert.equal(log.event, 'MigrationFinalized', 'wrong event initiated')
+        assert.equal(log.args.identity, proxy.address, 'finalized migrating wrong proxy')
+        assert.equal(log.args.newIdManager, newIdenManager.address, 'finalized migration to wrong location')
+        assert.equal(log.args.instigator, user1, 'finalized migrating from wrong user')
+        let data = '0x' + lightwallet.txutils._encodeFunctionTxData('register', ['uint256'], [LOG_NUMBER_1])
+        return newIdenManager.forwardTo(proxy.address, testReg.address, 0, data, {from: user1})
+      }).then(tx => {
+        // Verify that the proxy address is logged as the sender
+        return testReg.registry.call(proxy.address)
+      }).then((regData) => {
+        assert.equal(regData.toNumber(), LOG_NUMBER_1, 'User1 should be able to send transaction from new contract')
+        done()
+      })
+    })
+  })
 })
