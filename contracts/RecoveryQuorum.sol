bfc6a943ec40ad0677955825a831eb260b803f33
Joel Torstensson <oed3933@gmail.com>
2017-06-30 20:16:58 +0200

    Linting for RecoveryQuorum now works

--- contracts/RecoveryQuorum.sol
+++ contracts/RecoveryQuorum.sol
@@ -48,7 +48,7 @@ contract RecoveryQuorum {
     function changeUserKey(address newUserKey) public {
         if (collectedSignatures(newUserKey) >= neededSignatures()) {
             controller.changeUserKeyFromRecovery(newUserKey);
-            for (uint i = 0 ; i < delegateAddresses.length ; i++) {
+            for (uint i = 0; i < delegateAddresses.length; i++) {
                 //remove any pending delegates after a recovery
                 if (delegates[delegateAddresses[i]].pendingUntil > now) {
                     delegates[delegateAddresses[i]].deletedAfter = now;
@@ -59,22 +59,22 @@ contract RecoveryQuorum {
     }
 
     function replaceDelegates(address[] delegatesToRemove, address[] delegatesToAdd) onlyUserKey {
-        for (uint i = 0 ; i < delegatesToRemove.length; i++) {
+        for (uint i = 0; i < delegatesToRemove.length; i++) {
             removeDelegate(delegatesToRemove[i]);
         }
         garbageCollect();
-        for (uint j = 0 ; j < delegatesToAdd.length; j++) {
+        for (uint j = 0; j < delegatesToAdd.length; j++) {
             addDelegate(delegatesToAdd[j]);
         }
         RecoveryEvent("replaceDelegates", msg.sender);
     }
 
     function collectedSignatures(address _proposedUserKey) constant returns (uint signatures) {
-        for (uint i = 0 ; i < delegateAddresses.length ; i++) {
+        for (uint i = 0; i < delegateAddresses.length; i++) {
             if (delegateHasValidSignature(delegates[delegateAddresses[i]]) &&
                 delegates[delegateAddresses[i]].proposedUserKey == _proposedUserKey) {
                     signatures++;
-                }
+            }
         }
     }
 
@@ -84,8 +84,8 @@ contract RecoveryQuorum {
 
     function neededSignatures() constant returns (uint) {
         uint currentDelegateCount; //always 0 at this point
-        for(uint i = 0 ; i < delegateAddresses.length ; i++) {
-            if(delegateIsCurrent(delegates[delegateAddresses[i]])) {
+        for (uint i = 0; i < delegateAddresses.length; i++) {
+            if (delegateIsCurrent(delegates[delegateAddresses[i]])) {
                 currentDelegateCount++;
             }
         }
@@ -93,21 +93,21 @@ contract RecoveryQuorum {
     }
 
     function addDelegate(address delegate) private {
-        if(!delegateRecordExists(delegates[delegate])
-           && delegateAddresses.length < MAX_DELEGATES) {
+        if (!delegateRecordExists(delegates[delegate]) &&
+            delegateAddresses.length < MAX_DELEGATES) {
                delegates[delegate] = Delegate({
                    proposedUserKey: 0x0,
                    pendingUntil: now + controller.longTimeLock(),
                    deletedAfter: NEVER
                });
                delegateAddresses.push(delegate);
-           }
+       }
     }
 
     function removeDelegate(address delegate) private {
-        if(delegates[delegate].deletedAfter > controller.longTimeLock() + now) {
+        if (delegates[delegate].deletedAfter > controller.longTimeLock() + now) {
             //remove right away if they are still pending
-            if(delegates[delegate].pendingUntil > now) {
+            if (delegates[delegate].pendingUntil > now) {
                 delegates[delegate].deletedAfter = now;
             } else {
                 delegates[delegate].deletedAfter = controller.longTimeLock() + now;
@@ -117,11 +117,13 @@ contract RecoveryQuorum {
 
     function garbageCollect() private {
         uint i = 0;
-        while(i < delegateAddresses.length) {
-            if(delegateIsDeleted(delegates[delegateAddresses[i]])) {
+        while (i < delegateAddresses.length) {
+            if (delegateIsDeleted(delegates[delegateAddresses[i]])) {
                 delete delegates[delegateAddresses[i]];
                 ArrayLib.removeAddress(i, delegateAddresses);
-            } else { i++; }
+            } else {
+                i++;
+            }
         }
     }
 
