7264da845f64879cd7ec32aa7d9c168b50d808e5
Joel Torstensson <oed3933@gmail.com>
2017-09-26 10:54:45 +0200

    Build script now handles updates to artifacts

--- scripts/build-artifact-index.js
+++ scripts/build-artifact-index.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
+const assert = require('assert')
 const dirTree = require('directory-tree')
 const fs = require('fs')
 
+const CONTRACT_DIR = './build/contracts/'
+const VERSIONS_DIR = CONTRACT_DIR + 'versions/'
 
 function createArtifactIndex () {
-  const tree = dirTree('./build/contracts/')
+  const tree = dirTree(CONTRACT_DIR)
   let artifactIndex = { legacy: {} }
   let latestBuilds = {}
 
@@ -22,8 +25,7 @@ function createArtifactIndex () {
       }
     }
   }
-  console.log(latestBuilds)
-  console.log(artifactIndex)
+  artifactIndex = checkAndUpdateNewVersions(artifactIndex, latestBuilds)
   return artifactIndex
 
   function addLegacy (folder) {
@@ -53,6 +55,62 @@ function createArtifactIndex () {
       artifactIndex[name].latestVersion = version
       artifactIndex[name][version] = child.path
     }
+  }
+}
+
+function checkAndUpdateNewVersions (artifactIndex, latestBuilds) {
+  for (const build in latestBuilds) {
+    // ignore builds of artifacts we are not versioning
+    if (artifactIndex[build]) {
+      if (!isLatestVersion(build)) {
+        addNewVersion(build)
+      }
+    }
+  }
+  return artifactIndex
+
+  function addNewVersion (build) {
+    const newVersion = nextVersion(artifactIndex[build].latestVersion)
+    // create version dir if not present
+    const versionPath = VERSIONS_DIR + newVersion
+    if (!fs.existsSync(versionPath)) {
+      fs.mkdirSync(versionPath, '755')
+    }
+    // make a copy of the file to the correct version dir
+    const newArtifactPath = versionPath + `/${build}.json`
+    fs.copyFileSync('./' + latestBuilds[build], newArtifactPath)
+    // update artifactIndex
+    artifactIndex[build][newVersion] = newArtifactPath.slice(2)
+    artifactIndex[build].latestVersion = newVersion
+  }
+
+  function isLatestVersion (build) {
+    const buildArtifact = require('../' + latestBuilds[build])
+    const version = artifactIndex[build].latestVersion
+    const lvArtifact = require('../' + artifactIndex[build][version])
+
+    if (!isEqual(buildArtifact.abi, lvArtifact.abi)) return false
+
+    for (network in buildArtifact.networks) {
+      if (!lvArtifact.networks[network]) return false
+
+      if (buildArtifact.networks[network].address !== lvArtifact.networks[network].address) return false
+    }
+    return true
+  }
+
+  function isEqual (a, b) {
+    try {
+      assert.deepEqual(a,b)
+    } catch (e) {
+      return false
+    }
+    return true
+  }
+
+  function nextVersion(v) {
+    v = parseInt(v.slice(1))
+    return 'v' + ++v
   }
 }
 
