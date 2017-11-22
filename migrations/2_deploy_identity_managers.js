73525379b7e87a52c04cb9e72426321b69c23232
Joel Torstensson <oed3933@gmail.com>
2017-11-22 08:31:18 +0100

    Set timelocks in migration

--- migrations/2_deploy_identity_managers.js
+++ migrations/2_deploy_identity_managers.js
@@ -2,10 +2,9 @@ const IdentityManager = artifacts.require('./IdentityManager.sol')
 const TxRelay = artifacts.require('./TxRelay.sol')
 const MetaIdentityManager = artifacts.require('./MetaIdentityManager.sol')
 
-// TODO - specify these constants. They should also probably be different for different networks.
-const USER_TIME_LOCK = 0;
-const ADMIN_TIME_LOCK = 0;
-const ADMIN_RATE = 0;
+const USER_TIME_LOCK = 3600
+const ADMIN_TIME_LOCK = 129600
+const ADMIN_RATE = 1200
 
 module.exports = async function (deployer) {
   await deployer.deploy(IdentityManager, USER_TIME_LOCK, ADMIN_TIME_LOCK, ADMIN_RATE)
