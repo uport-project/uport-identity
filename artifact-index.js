7870b1c7eb867a04699fd9e6de16c8307a516b51
Joel Torstensson <oed3933@gmail.com>
2017-11-27 14:26:29 +0100

    Built artifact index

--- artifact-index.js
+++ artifact-index.js
@@ -3,27 +3,32 @@ module.exports = {
     ArrayLib: require('./build/contracts/legacy/ArrayLib.json'),
     IdentityFactory: require('./build/contracts/legacy/IdentityFactory.json'),
     IdentityFactoryWithRecoveryKey: require('./build/contracts/legacy/IdentityFactoryWithRecoveryKey.json'),
+    Owned: require('./build/contracts/legacy/Owned.json'),
     RecoverableController: require('./build/contracts/legacy/RecoverableController.json'),
     RecoveryQuorum: require('./build/contracts/legacy/RecoveryQuorum.json'),
   },
-  IdentityManager: {
+  Controlled: {
     latestVersion: 'v1',
+    v1: require('./build/contracts/versions/v1/Controlled.json'),
+  },
+  IdentityManager: {
+    latestVersion: 'v2',
     v1: require('./build/contracts/versions/v1/IdentityManager.json'),
+    v2: require('./build/contracts/versions/v2/IdentityManager.json'),
   },
   MetaIdentityManager: {
-    latestVersion: 'v1',
+    latestVersion: 'v2',
     v1: require('./build/contracts/versions/v1/MetaIdentityManager.json'),
-  },
-  Owned: {
-    latestVersion: 'v1',
-    v1: require('./build/contracts/versions/v1/Owned.json'),
+    v2: require('./build/contracts/versions/v2/MetaIdentityManager.json'),
   },
   Proxy: {
-    latestVersion: 'v1',
+    latestVersion: 'v2',
     v1: require('./build/contracts/versions/v1/Proxy.json'),
+    v2: require('./build/contracts/versions/v2/Proxy.json'),
   },
   TxRelay: {
-    latestVersion: 'v1',
+    latestVersion: 'v2',
     v1: require('./build/contracts/versions/v1/TxRelay.json'),
+    v2: require('./build/contracts/versions/v2/TxRelay.json'),
   }
 }
